a503a1f22f1894d89d1615fee542605d86ffb758
abargamin <bulldog_90@list.ru>
2015-05-09 11:42:19 +0300

    Changed name for first and second bikes. Fixed bug when Reset button doesn’t appear with the tilt controls.

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -39,7 +39,7 @@ public class GameSettings{
 	static float[] listTime_2 = {105, 160, 180, 70, 105, 75, 75, 85, 100, 115, 115, 105, 135, 135, 195};
 	static float[] listTime_1 = {120, 180, 200, 80, 120, 90, 90, 100, 115, 130, 130, 120, 150, 150, 210};
 	// set level for unlocking next bike
-	static int[] listUnlockingBike = {1,5,8,12}; // {1,2,4,12};
+	static int[] listUnlockingBike = {1,2,3,3}; // {1,2,4,12};
 
 	// speed, acceleration, lean, handling
 	static BikeStatics[] bikeStatisticsArray = {new BikeStatics(0.45f, 0.7f, 0.7f, 0.75f), 
--- Assets/Scripts/ShopNew.cs
+++ Assets/Scripts/ShopNew.cs
@@ -27,7 +27,7 @@ public class ShopNew : MonoBehaviour {
 	private int curBike;
 	private GameData data;
 
-	private string[] bikeNames = {"City Bike","Motocross Bike","Extreme Bike","Sport Bike","Bike 05","Bike 06"};
+	private string[] bikeNames = {"Motocross Bike","City Bike","Extreme Bike","Sport Bike","Bike 05","Bike 06"};
 	public static int[] prices = {0,20000,40000,65000,80000,100000};
 
 	private bool isAction = false;
