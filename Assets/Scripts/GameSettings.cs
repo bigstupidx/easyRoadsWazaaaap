cb2688f0c2102cfb2c5f630e424d8276b668b459
abargamin <bulldog_90@list.ru>
2015-05-22 20:10:11 +0300

    removed easy road objects from level 6.

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -35,9 +35,9 @@ public class GameSettings{
 	// set time for each level
 	public static float deltaGoodTime = 10f;
 
-	static float[] listTime_3 = {90, 140, 160, 120, 90, 58, 60, 70, 86, 102, 100, 90, 120, 120, 180};
-	static float[] listTime_2 = {105, 160, 180, 135, 105, 75, 75, 85, 100, 115, 115, 105, 135, 135, 195};
-	static float[] listTime_1 = {120, 180, 200, 150, 120, 90, 90, 100, 115, 130, 130, 120, 150, 150, 210};
+	static float[] listTime_3 = {90, 140, 160, 120, 90, 150, 60, 70, 86, 102, 100, 90, 120, 120, 180};
+	static float[] listTime_2 = {105, 160, 180, 135, 105, 160, 75, 85, 100, 115, 115, 105, 135, 135, 195};
+	static float[] listTime_1 = {120, 180, 200, 150, 120, 180, 90, 100, 115, 130, 130, 120, 150, 150, 210};
 	// set level for unlocking next bike
 	static int[] listUnlockingBike = {1,2,3,3}; // {1,2,4,12};
 
